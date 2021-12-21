14d0951130b360fc7a4e806117082fd1434c6800
Kindeme KOUNOUDJI <fkounoudji@gmail.com>
2021-12-21 01:09:40 -0600

    type conversion in java

--- FirstProject/src/com/training/mars/Main.java
+++ FirstProject/src/com/training/mars/Main.java
@@ -2,4 +2,17 @@ package com.training.mars;
 
 public class Main {
 
+	public static void main(String[] args) {
+	
+		// implicite conversion
+		
+		int i = 100;
+		long l = i; 
+		float f = l;
+		double d = 100.04;
+		int newInt = (int)d;
+		
+
+	}
+
 }
